1e5cd6b264068cddb5f726bf3cbde5267216c42d
GabrielHMA01 <159061439+GabrielHMA01@users.noreply.github.com>
2024-02-12 14:32:00 -0300

    colocar movimento no personagem

--- peixe.js
+++ peixe.js
@@ -13,64 +13,47 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload() {
-    //carrega a imagem de fundo
+
     this.load.image('Mar', 'assets/fundoMar.png');
-
-    //carrega a imagem das bolhas
     this.load.image('bolhas', 'assets/bolhas.png');
-
-    //carrega a imagem das algas
     this.load.image('algas', 'assets/Algas.png');
-
-    //carrega a logo do inteli
     this.load.image('logo', 'assets/logo-inteli_branco.png');
-
-    //carrega a imagem do peixe
     this.load.image('peixe', 'assets/peixe/tubarao.png');
-
-    //carrega a música de fundo
     this.load.audio('main_theme', 'assets/audio/musica_de_fundo.mp3');
     }
 
 function create () {
 
-    //adiciona música
-    var main_theme = this.sound.add("main_theme", {volume: 0.7} );
-
-    //dá play na música
-    main_theme.play();
-
-    //adiciona o fundo
     this.add.image(400, 300, 'Mar');
-
-    //adiciona a imagem das algas
     this.add.image(120, 500, 'algas') .setScale (0.12);
-
-    //adiciona a logo do inteli
     this.add.image(400, 75, 'logo') .setScale (0.5);
 
-    //guarda o tubarão em uma variável
     tubarao = this.add.image(400, 300, 'peixe');
+    bolhas = this.add.image(300, 300, 'bolhas') .setScale (0.2);
 
-    //guarda as bolhas em uma variável
-    bolhas = this.add.image(375, 250, 'bolhas') .setScale (0.2);
-
-    //flipa a imagem do tubarão
     tubarao.setFlip(true, false);
-
-    //flipa a iamgem das bolhas
     bolhas.setFlip(true, false);
+
+    keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+    keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+    keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     }
 
 function update () {
-    //adiciona movimento ao tubarão
-    tubarao.x = this.input.x;
-    tubarao.y = this.input.y;
+    var pixelMove = 10;
 
-    //adiciona movimentos às bolhas que acompanham o tubarão com um deslocamento no eixo X
-    bolhas.x = this.input.x-100;
-    bolhas.y = this.input.y;
+    if (keyA.isDown) {
+        tubarao.x -= pixelMove;
+        bolhas.x -= pixelMove;
+    } if (keyD.isDown) {
+        tubarao.x += pixelMove;
+        bolhas.x += pixelMove;
+    } if (keyS.isDown) {
+        tubarao.y += pixelMove;
+        bolhas.y += pixelMove;
+    } if (keyW.isDown) {
+        tubarao.y -= pixelMove;
+        bolhas.y -= pixelMove;
+    }
 }
-    //exibe mensagens antes de iniciar o jogo
-    window.alert ("Bem vindo!");
-    window.confirm ("Vamos começar? obs: O carregamento pode levar alguns instantes");
